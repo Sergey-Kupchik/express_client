c1140d01b62311881c98dc320c7a8a31b7613c6a
Sergey-Kupchik <kupchikrabota@gmail.com>
2021-08-09 12:54:47 -0400

    modify App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
+
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showCongrats, setShowCongrats] = useState(false)
+
+  const addUserHandler = () => {
+    axios({
+      url: 'http://localhost:5000/graphql',
+      method: 'POST',
+      data: {
+        query:`
+          mutation {
+            addUser (
+              userInput: {
+                email: "${email}", 
+                password: "${password}"
+              }){
+                _id
+                email
+                password
+              }
+        }
+        `
+      }
+    }).then((response) => {
+      console.log(response.data);
+      setEmail("");
+      setPassword("");
+      setShowCongrats(true);
+    }).catch((error) => console.error(error))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h5>Email</h5>
+      <input type="text" value={email} onChange={(e) => { setEmail(e.currentTarget.value) }}></input>
+      <h5>Password</h5>
+      <input type="text" value={password} onChange={(e) => { setPassword(e.currentTarget.value) }}></input>
+      <button style={{ display: "block", marginTop: "20px" }} disabled={!email.length && !password.length} onClick={addUserHandler}>Add User</button>
+      <div>
+        {showCongrats && "well done, you added new user to system"}
+      </div>
     </div>
   );
 }
